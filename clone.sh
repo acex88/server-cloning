3696d7f79059391bb6c174563455ad55f5768082
Abhinav Kumar <122148266+acex69@users.noreply.github.com>
2023-07-21 05:20:46 +0530

    clone.sh: fixup sepolicy vndr path

--- clone.sh
+++ clone.sh
@@ -5,6 +5,6 @@ git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common 
 git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
 git clone https://github.com/mt6768-dev/android_hardware_mediatek hardware/mediatek
 git clone https://github.com/ArrowOS/android_external_tinyxml external/tinyxml
-rm -rf device_mediatek_sepolicy_vndr
+rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://gitlab.com/ImSurajxD/clang-r450784d prebuilts/clang/host/linux-x86/clang-r450784d
