b364e0b03f5fed1f642866927fa2774ac9334ce0
Abhinav Kumar <122148266+acex69@users.noreply.github.com>
2023-07-20 21:21:53 +0530

    sh: clone trees

---
+++ clone.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/Burhanverse/device_xiaomi_lancelot -b blaze device/xiaomi/lancelot
+git clone https://github.com/orkunergun/device_xiaomi_mt6768-common device/xiaomi/mt6768-common
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
+git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
+git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
+git clone https://github.com/mt6768-dev/android_hardware_mediatek hardware/mediatek
+git clone https://github.com/ArrowOS/android_external_tinyxml external/tinyxml
+rm -rf device_mediatek_sepolicy_vndr
+git clone https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
+git clone https://gitlab.com/ImSurajxD/clang-r450784d prebuilts/clang/host/linux-x86/clang-r450784d
