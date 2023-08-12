5a5270bae8f456864d312fe87331ff8392471780
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-08-12 18:32:33 +0530

    clone.sh: Track Official Tree

--- clone.sh
+++ clone.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/acex69/device_xiaomi_lancelot device/xiaomi/lancelot
-git clone https://github.com/acex69/mt6768-common_fantastic device/xiaomi/mt6768-common
+git clone https://github.com/ProjectElixir-Devices/device_xiaomi_mt6768-common device/xiaomi/mt6768-common
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
 git clone https://github.com/acex88/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
