ed1dbab90625e421f816594d8330e950422d448b
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-08-12 18:00:21 +0530

    clone.sh: Track

--- clone.sh
+++ clone.sh
@@ -1,10 +1,9 @@
-git clone https://github.com/acex69/device_xiaomi_lancelot-arrow device/xiaomi/lancelot
+git clone https://github.com/acex69/device_xiaomi_lancelot device/xiaomi/lancelot
 git clone https://github.com/acex69/mt6768-common_fantastic device/xiaomi/mt6768-common
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
 git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
-git clone https://github.com/mt6768-dev/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
+git clone https://github.com/acex88/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
 git clone https://github.com/mt6768-dev/android_hardware_mediatek hardware/mediatek
-git clone https://github.com/ArrowOS/android_external_tinyxml external/tinyxml
 rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
 git clone https://gitlab.com/ImSurajxD/clang-r450784d prebuilts/clang/host/linux-x86/clang-r450784d
