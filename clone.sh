95106e2e44a8daa5cdcf4dab812b9a9b993b6d8a
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-08-19 09:39:47 +0530

    clone.sh: adapt

--- clone.sh
+++ clone.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/acex69/device_xiaomi_lancelot device/xiaomi/lancelot
-git clone https://github.com/ProjectElixir-Devices/device_xiaomi_mt6768-common device/xiaomi/mt6768-common
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
-git clone https://github.com/mt6768-dev/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
-git clone https://github.com/acex88/android_kernel_xiaomi_mt6768 kernel/xiaomi/mt6768
-git clone https://github.com/mt6768-dev/android_hardware_mediatek hardware/mediatek
-rm -rf device/mediatek/sepolicy_vndr
-git clone https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
-git clone https://gitlab.com/ImSurajxD/clang-r450784d prebuilts/clang/host/linux-x86/clang-r450784d
+git clone --depth 1 https://github.com/ProjectElixir-Devices/device_xiaomi_lancelot device/xiaomi/lancelot
+git clone --depth 1 https://github.com/ProjectElixir-Devices/device_xiaomi_mt6768-common device/xiaomi/mt6768-common
+git clone --depth 1 https://github.com/ProjectElixir-Devices/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
+git clone --depth 1 https://github.com/ProjectElixir-Devices/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
+git clone --depth 1 https://github.com/mt6768-dev/android_hardware_mediatek -b lineage-20 hardware/mediatek
+git clone --depth 1 https://github.com/acex88/kernel_mt6768-zepr kernel/xiaomi/mt6768
+git clone --depth 1 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
+git clone --depth 1 https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
+git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 prebuilts/clang/host/linux-x86/proton-clang 
