8d9e763a635f8c9dccee042a3cdc2893e9d27234
Abhinav Kumar (raizel) <122148266+acex88@users.noreply.github.com>
2023-09-09 21:43:20 +0530

    clone.sh: Track new kernel

--- clone.sh
+++ clone.sh
@@ -3,7 +3,7 @@ git clone --depth 1 https://github.com/ProjectElixir-Devices/device_xiaomi_mt676
 git clone --depth 1 https://github.com/ProjectElixir-Devices/proprietary_vendor_xiaomi_lancelot vendor/xiaomi/lancelot
 git clone --depth 1 https://github.com/ProjectElixir-Devices/proprietary_vendor_xiaomi_mt6768-common vendor/xiaomi/mt6768-common
 git clone --depth 1 https://github.com/mt6768-dev/android_hardware_mediatek -b lineage-20 hardware/mediatek
-git clone --depth 1 https://github.com/acex88/kernel_mt6768-zepr kernel/xiaomi/mt6768
+git clone --depth 1 https://github.com/raizeldumps/Kernel-Mi8745 -b   raizel kernel/xiaomi/mt6768
 git clone --depth 1 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
 git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 prebuilts/clang/host/linux-x86/proton-clang 
