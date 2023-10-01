1e8fad5021c33ec55b3b5bc7f259ccddf660a778
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-10-01 21:21:45 +0530

    clone.sh: Nuke sepolicy

--- clone.sh
+++ clone.sh
@@ -3,6 +3,7 @@ git clone --depth 1  https://github.com/crdroidandroid/android_device_xiaomi_mt6
 git clone --depth 1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_mt6768 vendor/xiaomi
 git clone --depth 1 https://github.com/mt6768-dev/android_hardware_mediatek -b lineage-20 hardware/mediatek
 git clone --depth 1 https://github.com/raizeldumps/Kernel-Mi8745 -b raizel kernel/xiaomi/mt6768
+rm -rf device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
 git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 prebuilts/clang/host/linux-x86/proton-clang 
