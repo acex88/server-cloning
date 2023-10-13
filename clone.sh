dbe16c6879deb992dc198a36f608f0ad4b99992f
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2023-10-13 22:24:13 +0530

    clone.sh: Include viper

--- clone.sh
+++ clone.sh
@@ -7,3 +7,4 @@ rm -rf device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/mt6768-dev/android_device_mediatek_sepolicy_vndr -b lineage-20 device/mediatek/sepolicy_vndr
 git clone --depth 1 https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
 git clone --depth 1 https://gitlab.com/LeCmnGend/proton-clang -b clang-16 prebuilts/clang/host/linux-x86/proton-clang 
+git clone https://github.com/acex88/ViPER4AndroidFX packages/apps/ViPER4AndroidFX
