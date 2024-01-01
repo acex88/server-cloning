a9972ad5a6c51cc798627b5d2c05e58a5e58d22e
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-01-01 12:46:31 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -5,5 +5,3 @@ git clone DroidX-UI/hardware_mediatek.git -rf 14 hardware/mediatek
 git clone --depth 1 https://gitlab.com/MT6768Lab/KernelTree kernel/xiaomi/mt6768
 rm -rf device/mediatek/sepolicy_vndr
 git clone https://github.com/SuperiorOS/android_device_mediatek_sepolicy_vndr device/mediatek/sepolicy_vndr
-git clone  https://gitlab.com/LeCmnGend/proton-clang -b clang-16 prebuilts/clang/host/linux-x86/proton-clang 
-git clone https://github.com/acex88/ViPER4AndroidFX packages/apps/ViPER4AndroidFX
